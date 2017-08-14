6a0217c0854aa0ee027221cf6b6e02a6f266940b
Naleen Balalla <linrules@gmail.com>
2017-08-14 12:46:17 +0800

    added a .NET 4.6 test project

---
+++ TransmaxGrading.2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TransmaxGrading.2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TransmaxGrading.2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57fcaffa-a6a2-4189-ad6d-f8fc4a0e6b05")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TransmaxGrading.Tests/NameChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TransmaxGrading.Tests
+{
+    [TestClass]
+    public class NameChanged
+    {
+        [TestMethod]
+        public void TestSomething()
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
